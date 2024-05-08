71cc9605b6e21b65b9779ff3e3571fe7cb037785
SkeelokJr <167640044+SkeelokJr@users.noreply.github.com>
2024-05-07 21:47:34 -0500

    Update MouseTest.py
    starts on "s", ends on "e"

--- MouseTest.py
+++ MouseTest.py
@@ -1,12 +1,22 @@
-# first open terminal and type "pip install pyautogui"
+# first open terminal and type "pip install pyautogui" and "pip install keyboard"
 
 import pyautogui as pag
+import keyboard as kb
+
+running = True
 
 screen_size = pag.size()
 screen_width, screen_height = screen_size[0], screen_size[1]
 screen_centerX, screen_centerY = screen_width/2, screen_height/2
 
 # move mouse to screen center
-pag.moveTo(screen_centerX, screen_centerY)
+def start():
+    pag.moveTo(screen_centerX, screen_centerY)
+    while True:
+        if kb.is_pressed('e'):
+            break
+        print("running")
 
-print(screen_centerX, screen_centerY)
+while True:
+    if kb.is_pressed('s'):
+        start()
