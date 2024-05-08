7fa1ab7398af03b8bfae910cbd7d5a376de50aec
SkeelokJr <167640044+SkeelokJr@users.noreply.github.com>
2024-05-07 20:28:35 -0500

    1

--- MouseTest.py
+++ MouseTest.py
@@ -1,4 +1,8 @@
 # first open terminal and type "pip3 install pyautogui and then pip3 install pygame"
+# download GitHub Remote Repositories and GitHub push and pull extensions, 
+# f1 to type "open remote repository" -> aiTest and open remote repository
+# go to source control and save file to push to remote repository
+
 import pyautogui as pag
 import pygame as pg
 
@@ -10,4 +14,3 @@ screen_centerX, screen_centerY = screen_width/2, screen_height/2
 pag.moveTo(screen_centerX, screen_centerY)
 
 print(screen_centerX, screen_centerY)
-#test
